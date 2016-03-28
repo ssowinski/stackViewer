73ceda1bcec8216f296960bc11df8852fcce0ace
Slawomir Sowinski <sl.sowinski@gmail.com>
2016-03-29 00:02:51 +0200

    Added share button and popover hint.

---
+++ stackViewer/HintViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HintViewController.swift
+//  stackViewer
+//
+//  Created by Slawomir Sowinski on 28.03.2016.
+//  Copyright © 2016 Slawomir Sowinski. All rights reserved.
+//
+
+import UIKit
+
+class HintViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
